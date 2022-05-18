6a54834facd91860929853c274781c455074b1b1
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-05-18 20:14:48 -0300

    insere componentes na pasta pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Album from './Album';
-import Favorites from './Favorites';
-import Login from './Login';
-import NotFound from './NotFound';
-import Profile from './Profile';
-import ProfileEdit from './ProfileEdit';
-import Search from './Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
---
+++ src/pages/Login.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
+
+class Login extends React.Component {
+  state = {
+    isButtonDisabled: true,
+    inputLenght: '',
+    isLoading: false,
+  }
+
+  valueInput = (event) => {
+    const { value } = event.target;
+    this.setState({ inputLenght: value }, this.buttonState);
+  }
+
+  buttonState = () => {
+    const { inputLenght } = this.state;
+    const caracteresInput = 3;
+    if (inputLenght.length >= caracteresInput) {
+      return this.setState(() => ({ isButtonDisabled: false }));
+    }
+    return this.setState(() => ({ isButtonDisabled: true }));
+  }
+
+  saveName = () => {
+    const { inputLenght } = this.state;
+    const { history } = this.props;
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      await createUser({ name: inputLenght });
+      history.push('/search');
+    });
+  }
+
+  render() {
+    const { isButtonDisabled, inputLenght, isLoading } = this.state;
+    return isLoading ? <Loading /> : (
+      <div data-testid="page-login">
+        <input
+          data-testid="login-name-input"
+          placeholder="Nome"
+          type="text"
+          name="inputName"
+          onChange={ this.valueInput }
+          value={ inputLenght }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ isButtonDisabled }
+          onClick={ this.saveName }
+        >
+          Entrar
+        </button>
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  history: PropTypes.func.isRequired,
+};
+
+export default Login;
