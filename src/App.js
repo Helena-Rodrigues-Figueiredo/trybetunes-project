d3c567a9cd525b707f97631ee65c4436b613586d
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-05-18 15:25:00 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Album from './Album';
+import Favorites from './Favorites';
+import Login from './Login';
+import NotFound from './NotFound';
+import Profile from './Profile';
+import ProfileEdit from './ProfileEdit';
+import Search from './Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes Helena</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/album/:id" component={ Album } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
