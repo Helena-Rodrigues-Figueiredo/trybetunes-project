d06cbaadacec3371cc099bddcf53c6c05d1f68b2
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-06-30 18:56:15 -0300

    Requsito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
-  render() {
-    return (
+state = {
+  userData: null,
+}
+
+componentDidMount = async () => {
+  const retornoApi = await getUser();
+  this.setState({
+    userData: retornoApi,
+  });
+}
+
+render() {
+  const { userData } = this.state;
+  return (
+    <>
       <div data-testid="page-profile">
         <Header />
       </div>
-    );
-  }
+      { userData === null
+        ? <Loading />
+        : (
+          <div>
+            <img
+              src={ userData.image }
+              alt="foto-usuario"
+              data-testid="profile-image"
+            />
+            <h1>{ userData.name }</h1>
+            <p>{ userData.email }</p>
+            <p>{ userData.description }</p>
+          </div>
+        )}
+      <Link to="/profile/edit">Editar perfil</Link>
+    </>
+  );
+}
 }
 
 export default Profile;
