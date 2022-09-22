bf49ee5fbf8084c9edda0d3c0fdb3f08e4132e84
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-09-22 17:13:09 -0300

    style da pag de login - media até 480px

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
 import { createUser } from '../services/userAPI';
-import dancingPeople from '../images/people-dancing-on-festival-4036162-3345485.webp';
+import dancingPeople from '../images/peopleDancing.webp';
+import dancingPerson from '../images/Music_Flatline.svg';
+import '../styles/Login.css';
 
 class Login extends React.Component {
   state = {
@@ -40,18 +42,21 @@ class Login extends React.Component {
     const { isButtonDisabled, inputLenght, isLoading } = this.state;
     return isLoading ? <Loading /> : (
       <>
+        <h1 id="login-trybetunes" className="login-title">TrybeTunes</h1>
         <div data-testid="page-login" id="login-page">
-          <h1 id="login-trybetunes">TrybeTunes</h1>
-          <input
-            id="login-input"
-            data-testid="login-name-input"
-            placeholder="Nome"
-            type="text"
-            name="inputName"
-            onChange={ this.valueInput }
-            value={ inputLenght }
-            autoComplete="off"
-          />
+          <img src={ dancingPerson } alt="person dancing" width={ 300 } />
+          <label htmlFor="login-input">
+            <input
+              id="login-input"
+              data-testid="login-name-input"
+              placeholder="Nome"
+              type="text"
+              name="inputName"
+              onChange={ this.valueInput }
+              value={ inputLenght }
+              autoComplete="off"
+            />
+          </label>
           <button
             id="login-button"
             type="submit"
@@ -61,8 +66,13 @@ class Login extends React.Component {
           >
             Entrar
           </button>
+          <img
+            src={ dancingPeople }
+            alt="person dancing"
+            width={ 250 }
+            id="dancing-people-img"
+          />
         </div>
-        <img src={ dancingPeople } alt="People-Dancing" />
       </>
     );
   }
