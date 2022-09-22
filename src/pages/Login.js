01f846c782be712a300a62a2a9d6e767122a50ba
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-09-22 14:57:42 -0300

    Adiciona imagem da pg de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
 import { createUser } from '../services/userAPI';
+import dancingPeople from '../images/people-dancing-on-festival-4036162-3345485.webp';
 
 class Login extends React.Component {
   state = {
@@ -38,28 +39,31 @@ class Login extends React.Component {
   render() {
     const { isButtonDisabled, inputLenght, isLoading } = this.state;
     return isLoading ? <Loading /> : (
-      <div data-testid="page-login" id="login-page">
-        <h1 id="login-trybetunes">TrybeTunes</h1>
-        <input
-          id="login-input"
-          data-testid="login-name-input"
-          placeholder="Nome"
-          type="text"
-          name="inputName"
-          onChange={ this.valueInput }
-          value={ inputLenght }
-          autoComplete="off"
-        />
-        <button
-          id="login-button"
-          type="submit"
-          data-testid="login-submit-button"
-          disabled={ isButtonDisabled }
-          onClick={ this.saveName }
-        >
-          Entrar
-        </button>
-      </div>
+      <>
+        <div data-testid="page-login" id="login-page">
+          <h1 id="login-trybetunes">TrybeTunes</h1>
+          <input
+            id="login-input"
+            data-testid="login-name-input"
+            placeholder="Nome"
+            type="text"
+            name="inputName"
+            onChange={ this.valueInput }
+            value={ inputLenght }
+            autoComplete="off"
+          />
+          <button
+            id="login-button"
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isButtonDisabled }
+            onClick={ this.saveName }
+          >
+            Entrar
+          </button>
+        </div>
+        <img src={ dancingPeople } alt="People-Dancing" />
+      </>
     );
   }
 }
--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,14 +1,68 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class ProfileEdit extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-profile-edit">
-        <Header />
-      </div>
-    );
-  }
+state = {
+  isLoading: true,
+  userData: null,
+  editData: {
+    name: '',
+    email: '',
+    description: '',
+  },
+}
+
+componentDidMount = async () => {
+  const result = await getUser();
+  this.setState({
+    userData: result,
+  });
+}
+
+render() {
+  return (
+    <div data-testid="page-profile-edit">
+      <Header />
+      <form>
+        <label htmlFor="nome">
+          Nome
+          <input type="text" data-testid="edit-input-name" placeholder="nome" id="nome" />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            type="text"
+            data-testid="edit-input-email"
+            placeholder="email"
+            id="nome"
+          />
+        </label>
+        <label htmlFor="descricao">
+          Descrição
+          <textarea
+            type="text"
+            data-testid="edit-input-name"
+            placeholder="Escreva sobre você"
+            id="descricao"
+          />
+        </label>
+        <label htmlFor="image">
+          Alterar imagem
+          <input
+            type="file"
+            data-testid="edit-input-image"
+            placeholder="imagem"
+            id="image"
+          />
+        </label>
+        <button type="button" data-testid="edit-button-save">
+          Salvar
+        </button>
+      </form>
+    </div>
+  );
+}
 }
 
 export default ProfileEdit;
